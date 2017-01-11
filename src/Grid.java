a8cf47b97e4e74c4e2fece9a840aa7c64f90d6be
vadym <garaschuck@gmail.com>
2017-01-11 23:30:03 +0200

    Commit

---
+++ src/Grid.java
@@ -0,0 +1,180 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by vadym on 04.01.17.
+ */
+public class Grid {
+
+    int[][]grid;
+
+    public Grid(int[][] list) {
+        grid = list;
+    }
+
+    public void start(){
+        createNumber();
+        createNumber();
+    }
+
+    public void showGrid()
+    {
+        String dot = ".";
+        for(int[] x: grid) {
+            for (int y: x) {
+                if (y == 0)
+                    System.out.printf("%3s ",".");
+                else
+                    System.out.printf("%3d ", y);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+    public int[][] createNumber()
+    {
+        Random random = new Random();
+        int rand_i = random.nextInt(grid.length);
+        int rand_j = random.nextInt(grid.length);
+        if (grid[rand_i][rand_j] == 0)
+            grid[rand_i][rand_j] = 2;
+        else
+            createNumber();
+
+        return grid;
+    }
+
+
+    public void moveUp() {
+
+        boolean allow = false;
+
+        for (int i = 0; i < grid.length; i++) {
+            int[] list = new int[grid.length];
+            for (int j = 0; j < grid.length; j++) {
+                list[j] = grid[j][i];
+            }
+            int[] t = processBEGINING(list);
+            if (!Arrays.equals(t,list))
+                allow = true;
+
+            for  (int z = 0; z < grid.length; z++) {
+                grid[z][i] = t[z];
+            }
+
+        }
+        if (allow)
+            createNumber();
+
+    }
+
+
+    public void moveDown() {
+        boolean allow = false;
+
+        for (int i = 0; i < grid.length; i++) {
+            int[] list = new int[grid.length];
+            for (int j = 0; j < grid.length; j++) {
+                list[j] = grid[j][i];
+            }
+            int[] t = processEND(list);
+            if (!Arrays.equals(t,list))
+                allow = true;
+            for  (int z = 0; z < grid.length; z++) {
+                grid[z][i] = t[z];
+            }
+
+        }
+        if(allow)
+            createNumber();
+    }
+
+
+    public void moveRight() {
+
+        boolean allow = false;
+        int[] temp;
+        for (int i = 0; i < grid.length; i++) {
+            temp = grid[i];
+            grid[i] = processEND(grid[i]);
+            if (!Arrays.equals(temp,grid[i]))
+                allow = true;
+        }
+        if (allow)
+            createNumber();
+    }
+
+    public void moveLeft() {
+        boolean allow = false;
+        int[] temp;
+        for (int i = 0; i < grid.length; i++) {
+            temp = grid[i];
+            grid[i] = processBEGINING(grid[i]);
+            if (!Arrays.equals(temp,grid[i]))
+                allow = true;
+        }
+        if (allow)
+            createNumber();
+    }
+
+
+
+
+
+    public  int[] processBEGINING(int[] list) {
+        int[] temp = new int[list.length];
+        int j = 0;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != 0)
+                temp[j++] = list[i];
+        }
+        for (int index = 1; index < temp.length; index++) {
+            if (temp[index - 1] == temp[index]) {
+                temp[index-1] *= 2;
+                temp[index] = 0;
+               }
+        }
+        for (int z = 1; z < temp.length; z++) {
+            if (temp[z-1] == 0) {
+                temp[z-1] = temp[z];
+                temp[z] = 0;
+            }
+        }
+
+        return temp;
+    }
+
+
+    public  int[] processEND(int[] list) {
+        int[] temp = new int[list.length];
+        int j = temp.length-1;
+        for (int i = list.length-1; i >= 0; i--) {
+            if (list[i] != 0)
+                temp[j--] = list[i];
+        }
+
+        for (int index = temp.length-1; index > 0; index--) {
+            if (temp[index] == temp[index - 1]) {
+                temp[index] *= 2;
+                temp[index - 1] = 0;
+
+            }
+        }
+
+        for (int z = temp.length-1; z > 0; z--) {
+            for (int i = z; i > 0; i--) {
+                if (temp[i] == 0) {
+                    int t = temp[i];
+                    temp[i] = temp[i-1];
+                    temp[i - 1] = t;
+                }
+            }
+        }
+        return temp;
+    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by vadym on 04.01.17.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        boolean b = true;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter size of your grid: ");
+        int n = scanner.nextInt();
+
+        int[][] temp = new int[n][n];
+        Grid g = new Grid(temp);
+        g.start();
+        g.showGrid();
+
+
+
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Please select direction(w/s/a/d): ");
+            String s = scanner.next();
+            switch (s) {
+                case "w":
+                    g.moveUp();
+                    g.showGrid();
+                    break;
+                case "s":
+                    g.moveDown();
+                    g.showGrid();
+                    break;
+                case "d":
+                    g.moveRight();
+                    g.showGrid();
+                    break;
+                case "a":
+                    g.moveLeft();
+                    g.showGrid();
+                    break;
+                default:
+                    System.out.println("Please, enter correct direction");
+
+
+            }
+
+            }
+
+
+        }
+
+    }
+
