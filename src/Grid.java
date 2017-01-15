20801c07c09cb201000361070d71667e888f3c11
vadym <garaschuck@gmail.com>
2017-01-15 16:05:54 +0200

    Commit Grid.java

--- src/Grid.java
+++ src/Grid.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
+
+import static java.util.Collections.reverse;
 
 /**
  * Created by vadym on 04.01.17.
@@ -49,6 +52,8 @@ public class Grid {
 
     public void moveUp() {
 
+
+
         boolean allow = false;
 
         for (int i = 0; i < grid.length; i++) {
@@ -79,7 +84,7 @@ public class Grid {
             for (int j = 0; j < grid.length; j++) {
                 list[j] = grid[j][i];
             }
-            int[] t = processEND(list);
+            int[] t = processBEGINING(list);
             if (!Arrays.equals(t,list))
                 allow = true;
             for  (int z = 0; z < grid.length; z++) {
@@ -147,33 +152,58 @@ public class Grid {
     }
 
 
-    public  int[] processEND(int[] list) {
-        int[] temp = new int[list.length];
-        int j = temp.length-1;
-        for (int i = list.length-1; i >= 0; i--) {
-            if (list[i] != 0)
-                temp[j--] = list[i];
-        }
 
-        for (int index = temp.length-1; index > 0; index--) {
-            if (temp[index] == temp[index - 1]) {
-                temp[index] *= 2;
-                temp[index - 1] = 0;
+    public int[] processEND(int[] list)
+    {
+        int[] temp = processBEGINING(reverse(list));
 
-            }
-        }
+        return reverse(temp);
 
-        for (int z = temp.length-1; z > 0; z--) {
-            for (int i = z; i > 0; i--) {
-                if (temp[i] == 0) {
-                    int t = temp[i];
-                    temp[i] = temp[i-1];
-                    temp[i - 1] = t;
-                }
-            }
-        }
-        return temp;
     }
+    
+    
+    
+    
+    public int[] reverse(int[] list)
+    {
+        int[] array = new int[list.length];
+        System.arraycopy(list,0,array,0,list.length);
+        for (int i = 0, j = array.length - 1; i < array.length/2; i++, j--) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
+
+
+//    public  int[] processEND(int[] list) {
+//        int[] temp = new int[list.length];
+//        int j = temp.length-1;
+//        for (int i = list.length-1; i >= 0; i--) {
+//            if (list[i] != 0)
+//                temp[j--] = list[i];
+//        }
+//
+//        for (int index = temp.length-1; index > 0; index--) {
+//            if (temp[index] == temp[index - 1]) {
+//                temp[index] *= 2;
+//                temp[index - 1] = 0;
+//
+//            }
+//        }
+//
+//        for (int z = temp.length-1; z > 0; z--) {
+//            for (int i = z; i > 0; i--) {
+//                if (temp[i] == 0) {
+//                    int t = temp[i];
+//                    temp[i] = temp[i-1];
+//                    temp[i - 1] = t;
+//                }
+//            }
+//        }
+//        return temp;
+//    }
 
 
 
