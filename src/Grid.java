5d832313b3ab7b20d430771b80450451a8bb7dab
Vadym Harashchuk <Vadym.Harashchuk@playtech.com>
2017-01-17 10:14:31 +0200

    add grid

--- src/Grid.java
+++ src/Grid.java
@@ -1,35 +1,31 @@
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Random;
-
-import static java.util.Collections.reverse;
 
 /**
  * Created by vadym on 04.01.17.
  */
 public class Grid {
 
-    int[][]grid;
+    int[][] grid;
 
-    public static int score =0;
+    public int score = 0;
 
     public Grid(int[][] list) {
         grid = list;
     }
 
-    public void start(){
+    public void start() {
         createNumber();
         createNumber();
     }
 
-    public void showGrid()
-    {
+    public void showGrid() {
         String dot = ".";
         System.out.println("Score: " + score);
-        for(int[] x: grid) {
-            for (int y: x) {
+        for (int[] x : grid) {
+            for (int y : x) {
                 if (y == 0)
-                    System.out.printf("%3s ",".");
+                    System.out.printf("%3s ", ".");
                 else
                     System.out.printf("%3d ", y);
             }
@@ -39,8 +35,7 @@ public class Grid {
     }
 
 
-    public int[][] createNumber()
-    {
+    public int[][] createNumber() {
 
 
         Random random = new Random();
@@ -55,11 +50,7 @@ public class Grid {
     }
 
 
-
-
     public void moveUp() {
-
-
 
         boolean allow = false;
 
@@ -68,11 +59,11 @@ public class Grid {
             for (int j = 0; j < grid.length; j++) {
                 list[j] = grid[j][i];
             }
-            int[] t = processBEGINING(list);
-            if (!Arrays.equals(t,list))
+            int[] t = process(list);
+            if (!Arrays.equals(t, list))
                 allow = true;
 
-            for  (int z = 0; z < grid.length; z++) {
+            for (int z = 0; z < grid.length; z++) {
                 grid[z][i] = t[z];
             }
 
@@ -91,15 +82,15 @@ public class Grid {
             for (int j = 0; j < grid.length; j++) {
                 list[j] = grid[j][i];
             }
-            int[] t = processEND(list);
-            if (!Arrays.equals(t,list))
+            int[] t = reverse(process(reverse(list)));
+            if (!Arrays.equals(t, list))
                 allow = true;
-            for  (int z = 0; z < grid.length; z++) {
+            for (int z = 0; z < grid.length; z++) {
                 grid[z][i] = t[z];
             }
 
         }
-        if(allow)
+        if (allow)
             createNumber();
     }
 
@@ -110,8 +101,8 @@ public class Grid {
         int[] temp;
         for (int i = 0; i < grid.length; i++) {
             temp = grid[i];
-            grid[i] = processEND(grid[i]);
-            if (!Arrays.equals(temp,grid[i]))
+            grid[i] = reverse(process(reverse(grid[i])));
+            if (!Arrays.equals(temp, grid[i]))
                 allow = true;
         }
         if (allow)
@@ -123,8 +114,8 @@ public class Grid {
         int[] temp;
         for (int i = 0; i < grid.length; i++) {
             temp = grid[i];
-            grid[i] = processBEGINING(grid[i]);
-            if (!Arrays.equals(temp,grid[i]))
+            grid[i] = process(grid[i]);
+            if (!Arrays.equals(temp, grid[i]))
                 allow = true;
         }
         if (allow)
@@ -132,10 +123,7 @@ public class Grid {
     }
 
 
-
-
-
-    public  int[] processBEGINING(int[] list) {
+    public int[] process(int[] list) {
         int[] temp = new int[list.length];
         int j = 0;
         for (int i = 0; i < list.length; i++) {
@@ -144,14 +132,14 @@ public class Grid {
         }
         for (int index = 1; index < temp.length; index++) {
             if (temp[index - 1] == temp[index]) {
-                temp[index-1] *= 2;
+                temp[index - 1] *= 2;
                 temp[index] = 0;
-                score += temp[index-1];
-               }
+                score += temp[index - 1];
+            }
         }
         for (int z = 1; z < temp.length; z++) {
-            if (temp[z-1] == 0) {
-                temp[z-1] = temp[z];
+            if (temp[z - 1] == 0) {
+                temp[z - 1] = temp[z];
                 temp[z] = 0;
             }
         }
@@ -160,59 +148,26 @@ public class Grid {
     }
 
 
+//
+//    public int[] processEND(int[] list)
+//    {
+//        int[] temp = process(reverse(list));
+//
+//        return reverse(temp);
+//
+//    }
 
-    public int[] processEND(int[] list)
-    {
-        int[] temp = processBEGINING(reverse(list));
 
-        return reverse(temp);
-
-    }
-    
-    
-    
-    
-    public int[] reverse(int[] list)
-    {
+    public int[] reverse(int[] list) {
         int[] array = new int[list.length];
-        System.arraycopy(list,0,array,0,list.length);
-        for (int i = 0, j = array.length - 1; i < array.length/2; i++, j--) {
+        System.arraycopy(list, 0, array, 0, list.length);
+        for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
         }
         return array;
     }
-
-
-//    public  int[] processEND(int[] list) {
-//        int[] temp = new int[list.length];
-//        int j = temp.length-1;
-//        for (int i = list.length-1; i >= 0; i--) {
-//            if (list[i] != 0)
-//                temp[j--] = list[i];
-//        }
-//
-//        for (int index = temp.length-1; index > 0; index--) {
-//            if (temp[index] == temp[index - 1]) {
-//                temp[index] *= 2;
-//                temp[index - 1] = 0;
-//
-//            }
-//        }
-//
-//        for (int z = temp.length-1; z > 0; z--) {
-//            for (int i = z; i > 0; i--) {
-//                if (temp[i] == 0) {
-//                    int t = temp[i];
-//                    temp[i] = temp[i-1];
-//                    temp[i - 1] = t;
-//                }
-//            }
-//        }
-//        return temp;
-//    }
-
 
 
 }
